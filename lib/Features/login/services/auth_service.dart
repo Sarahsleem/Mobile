6fa90bbaa69db128c5faa30a528bb8b4cf659291
Yara Afifi <yaraafifi106@gmail.com>
2024-04-16 00:02:27 +0200

    any edit

--- lib/Features/login/services/auth_service.dart
+++ lib/Features/login/services/auth_service.dart
@@ -1,44 +1,40 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
   Future<UserCredential?> signInWithGoogle() async {
     try {
-      // Trigger the Google sign-in flow
-      final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
-      // Handle the case when user cancels the sign-in operation
-      if (gUser == null) {
+      if (googleUser == null) {
+        // User cancelled the sign-in operation
         print('Sign-in operation cancelled by the user.');
         return null;
       }
 
-      // Get the authentication credentials
-      final GoogleSignInAuthentication gAuth = await gUser.authentication;
+      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
 
-      // Create a credential from the access token and ID token
       final credential = GoogleAuthProvider.credential(
-        accessToken: gAuth.accessToken,
-        idToken: gAuth.idToken,
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
       );
 
-      // Sign in to Firebase with the credential
       return await FirebaseAuth.instance.signInWithCredential(credential);
+    } on PlatformException catch (e) {
+      if (e.code == 'sign_in_failed') {
+        // Handle sign-in failure
+        print('Error signing in with Google: ${e.message}. Please try again later.');
+        return null;
+      } else {
+        // Handle other PlatformExceptions
+        print('An error occurred: ${e.message}');
+        return null;
+      }
     } catch (e) {
-      // Handle any errors that occur during the sign-in process
+      // Handle other exceptions
       print('Error signing in with Google: $e');
       return null;
     }
   }
 }
-
-// Example of how to call the signInWithGoogle method
-// onPressed: () async {
-//   UserCredential? userCredential = await AuthService().signInWithGoogle();
-//   if (userCredential != null) {
-//     // Sign-in successful, navigate to the next screen
-//     GoRouter.of(context).push(AppRouter.kHomeView);
-//   } else {
-//     // Sign-in failed, handle accordingly (e.g., show error message)
-//   }
-// },
