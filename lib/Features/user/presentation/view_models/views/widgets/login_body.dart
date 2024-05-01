e31eb0d8a117ee6172941f14363b2441dc660822
Farah Seyam <farahseyam1911@gmail.com>
2024-05-01 22:13:32 +0300

    edit loading in login fist_trial

--- lib/Features/user/presentation/view_models/views/widgets/login_body.dart
+++ lib/Features/user/presentation/view_models/views/widgets/login_body.dart
@@ -3,6 +3,7 @@ import 'package:bookly_application/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_progress_hud/flutter_progress_hud.dart';
 import 'package:go_router/go_router.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 import '../../../../../../constants.dart';
 import 'custom_checktile.dart';
@@ -21,53 +22,65 @@ class _LoginState extends State<LoginScreen> {
   TextEditingController emailCont = TextEditingController();
 
   TextEditingController passCont = TextEditingController();
-
+  bool isLoading = false;
   var formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Builder(
-      builder: (context) => Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12.0),
-        child: Form(
-          key: formKey,
-          child: Column(
-            children: [
-              CustomTextField(
-                icon: Icons.mail,
-                hintText: 'Email',
-                cont: emailCont,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              CustomField(
-                hintText: 'Password',
-                cont: passCont,
-              ),
-              /* CustomChecktile(
-                preText: 'Remember me',
-                sufText: 'Forget password?',
-                onTap: () {
-                  GoRouter.of(context).push('/ForgetPass');
-                },
-              ), */
-              const SizedBox(
-                height: 50,
-              ),
-              CustomButon(
-                text: 'Login',
-                onTap: () {
-                  if (formKey.currentState!.validate()) {
-                    LoginUserApi().userLogin(
-                        context: context,
-                        emailcont: emailCont,
-                        passcont: passCont);
-                    // GoRouter.of(context).push(AppRouter.kHomeView);
-                  }
-                },
-              ),
-            ],
+      builder: (context) => ModalProgressHUD(
+        inAsyncCall: isLoading,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12.0),
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                CustomTextField(
+                  icon: Icons.mail,
+                  hintText: 'Email',
+                  cont: emailCont,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                CustomField(
+                  hintText: 'Password',
+                  cont: passCont,
+                ),
+                /* CustomChecktile(
+                  preText: 'Remember me',
+                  sufText: 'Forget password?',
+                  onTap: () {
+                    GoRouter.of(context).push('/ForgetPass');
+                  },
+                ), */
+                const SizedBox(
+                  height: 50,
+                ),
+                CustomButon(
+                  text: 'Login',
+                  onTap: () {
+                    if (formKey.currentState!.validate()) {
+                      print('login');
+                     
+                     
+                      setState(() { isLoading = true;});
+                       print('$isLoading');
+                       LoginUserApi().userLogin(
+                          context: context,
+                          emailcont: emailCont,
+                          passcont: passCont);
+                      
+                      
+                      setState(() {isLoading = false;}); 
+                       print('$isLoading');
+                      // GoRouter.of(context).push(AppRouter.kHomeView);
+                    }
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
